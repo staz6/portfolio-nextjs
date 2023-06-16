08c95fe5529ac26a5c431db691a630a0fb99c73d
staz6 <muhammad.aahad81@gmail.com>
2023-06-17 02:41:43 +0500

    chore: further compartmentalize code

---
+++ src/components/PortfolioSection/index.tsx
@@ -0,0 +1,57 @@
+import ProjectContainer from "./ProjectContainer";
+
+export default function PortfolioSection() {
+  const projects = [
+    {
+      src: "ih-1.webp",
+      position: "center",
+      stack: ["React", "Nodejs", "Firebase", "AWS"],
+      desc: "Directing the engineering of an advanced e-learning platform.",
+    },
+    {
+      src: "sc-1.webp",
+      position: "center",
+      stack: ["React", ".Net", "Azure Function"],
+      desc: "Crafting a low-cost, impactful solution for Toronto’s “Storage Connect” charity initiative.",
+    },
+    {
+      src: "cd-1.webp",
+      position: "top",
+      stack: ["React", "Nestjs", "Shopify"],
+      desc: "Streamlining and optimizing the website and infrastructure for enhanced performance.",
+    },
+    {
+      src: "occu-1.webp",
+      position: "top",
+      stack: ["React", "Lambda Function", "AWS Cognito"],
+      desc: "Delivered an insurance claim solution for Quality Claims, now in use by 300+ employees.",
+    },
+  ];
+
+  return (
+    <div className="container-fluid relative z-10">
+      <h2 className="m-0 mb-[15px] mt-[126px] p-0 font-[bold] text-2xl leading-6 text-[#87BA34]">
+        Portfolio
+      </h2>
+      <div className="gap-4 lg:grid lg:grid-cols-12">
+        <div className="lg:col-span-4">
+          <p className="m-0 p-0 text-lg leading-6 text-white">
+            Some of my most memorable projects highlight
+          </p>
+        </div>
+        <div className="flex flex-col items-end gap-10 lg:col-span-8">
+          {projects.map((project, index) => (
+            <ProjectContainer
+              // eslint-disable-next-line react/no-array-index-key
+              key={index}
+              src={project.src}
+              position={project.position}
+              stack={project.stack}
+              desc={project.desc}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/SkillsDescriptionSection/index.tsx
+++ src/components/SkillsDescriptionSection/index.tsx
@@ -8,61 +8,77 @@ import TSBackgroundIcon from "../icons/TSBackgroundIcon";
 import SkillBlock from "./SkillBlock";
 
 export default function SkillDescriptionSection() {
+  const skills = [
+    {
+      title: "React",
+      iconPath: "react-icon-green.png",
+      backgroundIcon: ReactBackgroundIcon,
+      contentText:
+        "Used in numerous projects, from AI-powered e-learning platforms to charity initiatives. I enjoy using its component-based architecture to build interactive user interfaces.",
+    },
+    {
+      title: "Node",
+      iconPath: "nodejs-icon-green.png",
+      backgroundIcon: NodeBackgroundIcon,
+      contentText:
+        "Leveraged in various projects to develop fast, scalable network applications. My experience extends from creating APIs to handling database operations.",
+      reverse: true,
+    },
+    {
+      title: "Next.js",
+      iconPath: "nextjs-icon-green.png",
+      backgroundIcon: NextjsBackgroundIcon,
+      contentText:
+        "Employed in several applications, appreciating its capabilities for server-side rendering, static site generation, and API routes.",
+    },
+    {
+      title: "Typescript",
+      iconPath: "ts-icon-green.png",
+      backgroundIcon: TSBackgroundIcon,
+      contentText:
+        "Utilized in numerous JavaScript projects to enhance development efficiency, improve readability, and catch errors early in the development process.",
+      reverse: true,
+    },
+    {
+      title: "Tailwind",
+      iconPath: "tailwind-icon-bg.png",
+      backgroundIcon: TailwindBackgroundIcon,
+      imageClass: "top-[150px] left-[158px]",
+      contentText:
+        "Used frequently to create custom user interfaces, appreciating its utility-first approach that enables rapid UI development.",
+    },
+    {
+      title: "AWS (DevOps)",
+      iconPath: "aws-icon-green.png",
+      backgroundIcon: AWSBackgroundIcon,
+      contentText:
+        "Regularly employed for application hosting, managing databases, and handling user authentication. I've worked extensively with services like Lambda, Cognito, and DynamoDB.",
+      reverse: true,
+    },
+    {
+      title: ".NET",
+      iconPath: "net-icon-green.png",
+      backgroundIcon: NetBackgroundIcon,
+      contentText:
+        "Began my career with .NET, contributing to the development of an ERP solution using a microservices architecture. I've also used it in several other backend applications.",
+    },
+  ];
+
   return (
     <div className="relative z-[50] w-full">
       <div className="container-fluid skills-description--section">
-        <SkillBlock
-          title="React"
-          iconPath="react-icon-green.png"
-          backgroundIcon={ReactBackgroundIcon}
-          contentText="Used in numerous projects, from AI-powered news feed generation to charity initiatives. I enjoy using its component-based architecture to build interactive user interfaces."
-        />
-
-        <SkillBlock
-          title="Node"
-          iconPath="nodejs-icon-green.png"
-          backgroundIcon={NodeBackgroundIcon}
-          contentText="Leveraged in various projects to develop fast, scalable network applications. My experience extends from creating APIs to handling database operations."
-          reverse
-        />
-
-        <SkillBlock
-          title="NEXTjs"
-          iconPath="nextjs-icon-green.png"
-          backgroundIcon={NextjsBackgroundIcon}
-          contentText="Employed in several applications, appreciating its capabilities for server-side rendering, static site generation, and API routes."
-        />
-
-        <SkillBlock
-          title="Typescript"
-          iconPath="ts-icon-green.png"
-          backgroundIcon={TSBackgroundIcon}
-          contentText="Utilized in numerous JavaScript projects to enhance development efficiency, improve readability, and catch errors early in the development proces."
-          reverse
-        />
-
-        <SkillBlock
-          title="Tailwind"
-          iconPath="tailwind-icon-bg.png"
-          backgroundIcon={TailwindBackgroundIcon}
-          imageClass="top-[150px] left-[158px]"
-          contentText="Used frequently to create custom user interfaces, appreciating its utility-first approach that enables rapid UI development."
-        />
-
-        <SkillBlock
-          title="AWS (DevOps)"
-          iconPath="aws-icon-green.png"
-          backgroundIcon={AWSBackgroundIcon}
-          contentText="Regularly employed for application hosting, managing databases, and handling user authentication. I've worked extensively with services like Lambda, Cognito, and DynamoDB."
-          reverse
-        />
-
-        <SkillBlock
-          title=".NET"
-          iconPath="net-icon-green.png"
-          backgroundIcon={NetBackgroundIcon}
-          contentText="Began my career with .NET, contributing to the development of an ERP solution using a microservices architecture. I've also used it in several other backend applications."
-        />
+        {skills.map((skill, index) => (
+          <SkillBlock
+            // eslint-disable-next-line react/no-array-index-key
+            key={index}
+            title={skill.title}
+            iconPath={skill.iconPath}
+            backgroundIcon={skill.backgroundIcon}
+            contentText={skill.contentText}
+            reverse={skill.reverse}
+            imageClass={skill.imageClass}
+          />
+        ))}
       </div>
     </div>
   );
